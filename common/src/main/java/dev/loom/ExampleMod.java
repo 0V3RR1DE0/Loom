d35f97e2ae5a27cbc7fd2bd7db754caba35fa7ac
0v3rr1de0 <fizzwareminecraft@gmail.com>
2026-07-28 13:41:50 +0300

    Initial Architectury multiplatform template, MC 1.21.10

---
+++ common/src/main/java/dev/loom/ExampleMod.java
@@ -0,0 +1,9 @@
+package dev.loom;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "loom";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
